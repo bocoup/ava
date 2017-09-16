b02472fdc82de6700de93e82ad895cc32e442090
Mike Pennisi <mike@mikepennisi.com>
2017-09-16 16:14:05 -0400

    fixup! Re-factor error object processing

--- test/helper/error-from-worker.js
+++ test/helper/error-from-worker.js
@@ -2,19 +2,21 @@
 
 const serializeError = require('../../lib/serialize-error');
 
-module.exports = function (err, {type, file, stack} = {}) {
-	if (stack) {
-		err.stack = stack;
+module.exports = function (err, options) {
+	options = Object.assign({}, options);
+
+	if (options.stack) {
+		err.stack = options.stack;
 	}
 
 	const serialized = serializeError(err);
 
-	if (type) {
-		serialized.type = type;
+	if (options.type) {
+		serialized.type = options.type;
 	}
 
-	if (file) {
-		serialized.file = file;
+	if (options.file) {
+		serialized.file = options.file;
 	}
 
 	return serialized;
